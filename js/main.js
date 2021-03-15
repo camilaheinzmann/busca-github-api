430000622c56750446dd23a02ea315b9ad96f1be
Camila Heinzmann <camilapostai.ch@gmail.com>
2021-03-15 00:43:34 -0300

    create responsive and stylized  interface

--- js/main.js
+++ js/main.js
@@ -1,54 +1,58 @@
-var inputElement = document.querySelector('#busca input');
-var buttonElement = document.querySelector('#busca button');
-var listElement = document.querySelector('#repos ul');
+var inputElement = document.querySelector("#busca input");
+var buttonElement = document.querySelector("#busca button");
+var listElement = document.querySelector("#repos ul");
 
 function msgErro() {
-    listElement.innerHTML = "";
+  listElement.innerHTML = "";
 
-    var errorElement = document.createElement('li');
-    var errorText = document.createTextNode("Usuário não encontrado!");
+  var errorElement = document.createElement("li");
+  var errorText = document.createTextNode("Usuário não encontrado!");
 
-    errorElement.appendChild(errorText);
-    listElement.appendChild(errorElement);
+  errorElement.appendChild(errorText);
+  listElement.appendChild(errorElement);
 }
 
 function msgCarregando() {
-    listElement.innerHTML = "";
+  listElement.innerHTML = "";
 
-    var carregElement = document.createElement('li');
-    var carregText = document.createTextNode("Carregando...");
+  var carregElement = document.createElement("li");
+  var carregText = document.createTextNode("Carregando...");
 
-    carregElement.appendChild(carregText);
-    listElement.appendChild(carregElement);
+  carregElement.appendChild(carregText);
+  listElement.appendChild(carregElement);
 }
 
 var listaRepos = function (repos) {
-    listElement.innerHTML = "";
+  listElement.innerHTML = "";
 
-    for (repo in repos) {
-        var repoElement = document.createElement('li');
+  for (repo in repos) {
+    var repoElement = document.createElement("li");
 
-        var linkRepo = document.createElement('a');
-        linkRepo.setAttribute('href', repos[repo].html_url);
-        linkRepo.setAttribute('target', '_blank');
+    var linkRepo = document.createElement("a");
+    linkRepo.setAttribute("href", repos[repo].html_url);
+    linkRepo.setAttribute("target", "_blank");
 
-        var repoText = document.createTextNode(repos[repo].name);
+    var btnText = document.createTextNode("Ir");
 
-        linkRepo.appendChild(repoText);
-        repoElement.appendChild(linkRepo);
-        listElement.appendChild(repoElement);
-    }
-}
+    var repoText = document.createTextNode(repos[repo].name);
+
+    linkRepo.appendChild(btnText);
+    repoElement.appendChild(repoText);
+    repoElement.appendChild(linkRepo);
+    listElement.appendChild(repoElement);
+  }
+};
 
 function buscaGit() {
-    var user = inputElement.value;
-    axios.get('https://api.github.com/users/' + user + '/repos')
-        .then(function (response) {
-            msgCarregando();
-            var repos = response.data;
-            listaRepos(repos);
-        })
-        .catch(function (error) {
-            msgErro();
-        });
+  var user = inputElement.value;
+  axios
+    .get("https://api.github.com/users/" + user + "/repos")
+    .then(function (response) {
+      msgCarregando();
+      var repos = response.data;
+      listaRepos(repos);
+    })
+    .catch(function (error) {
+      msgErro();
+    });
 }
